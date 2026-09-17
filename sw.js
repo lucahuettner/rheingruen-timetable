b8dffd64718b37d91a58c0e34482c85f6ccf7fd4
Luca Huettner <luca.huettner@gmail.com>
2026-09-17 21:33:16 +0200

    Make time axis column more compact on mobile

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-First Strategy with Network Fallback
  */
 
-const CACHE_NAME = "rheingruen-timetable-v15";
+const CACHE_NAME = "rheingruen-timetable-v16";
 
 const PRECACHE_ASSETS = [
   "./",
