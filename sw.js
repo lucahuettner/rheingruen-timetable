fb45dc82f3fff8edc8c52b73815e424b1a46d6aa
Luca Huettner <luca.huettner@gmail.com>
2026-09-18 21:14:08 +0200

    chore: remove og:image to exclude logo from link preview

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-First Strategy with Network Fallback
  */
 
-const CACHE_NAME = "rheingruen-timetable-v36";
+const CACHE_NAME = "rheingruen-timetable-v37";
 
 const PRECACHE_ASSETS = [
   "./",
